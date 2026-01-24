f2f0112e27ebeb1695329c422b8ddb4fe16a35fc
zaid <zaid.abdelmoumen@1glink.fr>
2026-01-24 16:31:48 +0100

    v3.2.3 pagesback

--- back-diet/backend/demo/src/main/java/com/recipe_service/demo/recipe/RecipeRepository.java
+++ back-diet/backend/demo/src/main/java/com/recipe_service/demo/recipe/RecipeRepository.java
@@ -1,7 +1,5 @@
 package com.recipe_service.demo.recipe;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
